e0331c6d9065cbad86f91264ef5c2f0ac6fbe1cf
Gilberto López <gilisaac@ciencias.unam.mx>
2016-04-21 23:22:47 -0500

    Correcciones BisectionMethod.java, MatrixInverse.java, RungeKutta4.java

--- BisectionMethod.java
+++ BisectionMethod.java
@@ -4,42 +4,42 @@ import java.util.*;
 public class BisectionMethod{
 
     public static void main(String[] args){
-	Scanner in = new Scanner(System.in);
-	//Input a,b
-	String[] interval = in.nextLine().split(",");
-	//Interval [a,b]
-	double a = Double.valueOf(interval[0]);
-	double b = Double.valueOf(interval[1]);
-	//f(x) = Cos(x) - x
-	//If f(a)*f(b) > 0 then there is no solution
-	if((Math.cos(a) - a) * (Math.cos(b) - b) > 0){
-	    System.out.println("No found");   
-	}else{
-	    double x0 = 0.0; //The solution, 0.0 to initialize the varaible.
-	    double e = Math.abs(b-a); //Error through iterations.
-	    double epsilon = 0.0001; //Error margin
-	    while(e > epsilon){
-		double c = (a + b)/2; //Midpoint between a and b
-		double fa = Math.cos(a) - a; //f(a)
-		double fb = Math.cos(b) - b; //f(b)
-		double fc = Math.cos(c) - c; //f(c)
-		if(fa == 0.0){
-		    x0 = a; //a is solution
-		    break;
-		}else if(fb == 0.0){
-		    x0 = b; //b is solution
-		    break;
-		}else if(fa*fc < 0.0){
-		    b = c; //The solution is in [a,c]
+		Scanner in = new Scanner(System.in);
+		//Input a,b
+		String[] interval = in.nextLine().split(",");
+		//Interval [a,b]
+		double a = Double.valueOf(interval[0]);
+		double b = Double.valueOf(interval[1]);
+		//f(x) = Cos(x) - x
+		//If f(a)*f(b) > 0 then there is no solution
+		if((Math.cos(a) - a) * (Math.cos(b) - b) > 0){
+		    System.out.println("No found");   
 		}else{
-		    a = c; //The solution is in [c,b]
+		    double x0 = 0.0; //The solution, 0.0 to initialize the varaible.
+		    double e = Math.abs(b-a); //Error through iterations.
+		    double epsilon = 0.0001; //Error margin
+		    while(e > epsilon){
+			double c = (a + b)/2; //Midpoint between a and b
+			double fa = Math.cos(a) - a; //f(a)
+			double fb = Math.cos(b) - b; //f(b)
+			double fc = Math.cos(c) - c; //f(c)
+			if(fa == 0.0){
+			    x0 = a; //a is solution
+			    break;
+			}else if(fb == 0.0){
+			    x0 = b; //b is solution
+			    break;
+			}else if(fa*fc < 0.0){
+			    b = c; //The solution is in [a,c]
+			}else{
+			    a = c; //The solution is in [c,b]
+			}
+			double cn = (a + b)/2; //Midpoint between the new a and new b
+			e = Math.abs((cn-c)/cn*100); //The relative error
+			x0 = c; //The aproximation of the solution at this iteration
+		    }
+		    System.out.printf("%.5f\n",x0);//Prints the solution
 		}
-		double cn = (a + b)/2; //Midpoint between the new a and new b
-		e = Math.abs((cn-c)/cn*100); //The relative error
-		x0 = c; //The aproximation of the solution at this iteration
-	    }
-	    System.out.printf("%.5f\n",x0);//Prints the solution
-	}
     }
 }
 
--- MatrixInverse.java
+++ MatrixInverse.java
@@ -15,10 +15,10 @@ public class MatrixInverse{
      * @return The resulting row.
      */
     public static double[] multiplyRow(double[][] A, int row, double x){
-	double[] r = new double[A[row].length];
-	for(int i = 0; i < A[row].length; i ++)
-	    r[i] = A[row][i] * x;
-	return r;
+		double[] r = new double[A[row].length];
+		for(int i = 0; i < A[row].length; i ++)
+		    r[i] = A[row][i] * x;
+		return r;
     }
 
     /**
@@ -29,68 +29,67 @@ public class MatrixInverse{
      * @param j The jth row of A.
      * @return The resulting row.
      */
-
     public static double[] addRows(double[][] A, int i, int j){
-	double[] r = new double[A[i].length];
-	for(int k = 0; k < r.length; k++)
-	    r[k] = A[i][k] + A[j][k];
-	return r;
+		double[] r = new double[A[i].length];
+		for(int k = 0; k < r.length; k++)
+		    r[k] = A[i][k] + A[j][k];
+		return r;
     }
 
     /**
      * The main method.
      */
     public static void main(String[] args){
-	Scanner in = new Scanner(System.in);
-	//Input: Matrix A
-	ArrayList<String[]> A = new ArrayList<>();
-	int n = 0; //The number of columns/rows of A.
-	while(in.hasNextLine()){
-	    String[] r = in.nextLine().split(" "); //ith row of A.
-	    A.add(r);
-	    n++;
-	    if(r.length == n) //The number of columns equals the number of rows.
-		break;
-	}
-	//Matrix [A|I]
-	double[][] AI = new double[n][2*n];
-	for(int j = 0; j < n; j++){
-	    for(int i = 0; i < n; i++){
-	    //Matrix A
-		AI[j][i] = Double.valueOf((A.get(j))[i]);
-	    }
-	}
-	//Identity Matrix
-	for(int k = 0; k < n; k++){
-	    AI[k][k+n] = 1.0;
-	}
-	//Now AI = [A|I]
-	for(int i = 0; i < n; i++){ //i The column of AI
-	    for(int j = 0; j < n; j++){ //j The row of AI
-		double Aji = AI[j][i];
-		//Putting 0 out of the diagonal of A
-		if(j != i && Aji != 0){
-		    if(AI[i][i] == 0){
-			AI[i] = addRows(AI, i, n-1);
-		    }
-		    AI[j] = multiplyRow(AI, j, -AI[i][i]/Aji);
-		    AI[j] = addRows(AI, j, i);
+		Scanner in = new Scanner(System.in);
+		//Input: Matrix A
+		ArrayList<String[]> A = new ArrayList<>();
+		int n = 0; //The number of columns/rows of A.
+		while(in.hasNextLine()){
+		    String[] r = in.nextLine().split(" "); //ith row of A.
+		    A.add(r);
+		    n++;
+		    if(r.length == n) //The number of columns equals the number of rows.
+				break;
 		}
-	    }
-	}
-	//Putting 1 on the diagonal of AI
-	for(int i = 0; i < n; i++)
-	    AI[i] = multiplyRow(AI, i, 1/AI[i][i]);
-	//Now AI = [I|A^-1]
-	for(int i = 0; i < n; i++){
-	    for(int j = 0; j < n; j++){
-		//Printig A^-1
-		System.out.printf("%.3f ", AI[i][j+n] + 0.0);
-		//Printing 3 decimal places
-		//+ 0.0 to avoid negative 0 on uoutput
-	    }
-	    System.out.print("\n");
-	}
+		//Matrix [A|I]
+		double[][] AI = new double[n][2*n];
+		for(int j = 0; j < n; j++){
+		    for(int i = 0; i < n; i++){
+			    //Matrix A
+				AI[j][i] = Double.valueOf((A.get(j))[i]);
+		    }
+		}
+		//Identity Matrix
+		for(int k = 0; k < n; k++){
+		    AI[k][k+n] = 1.0;
+		}
+		//Now AI = [A|I]
+		for(int i = 0; i < n; i++){ //i The column of AI
+		    for(int j = 0; j < n; j++){ //j The row of AI
+			double Aji = AI[j][i];
+			//Putting 0 out of the diagonal of A
+			if(j != i && Aji != 0){
+			    if(AI[i][i] == 0){
+				AI[i] = addRows(AI, i, n-1);
+			    }
+			    AI[j] = multiplyRow(AI, j, -AI[i][i]/Aji);
+			    AI[j] = addRows(AI, j, i);
+			}
+		    }
+		}
+		//Putting 1 on the diagonal of AI
+		for(int i = 0; i < n; i++)
+		    AI[i] = multiplyRow(AI, i, 1/AI[i][i]);
+		//Now AI = [I|A^-1]
+		for(int i = 0; i < n; i++){
+		    for(int j = 0; j < n; j++){
+			//Printig A^-1
+			System.out.printf("%.3f ", AI[i][j+n] + 0.0);
+			//Printing 3 decimal places
+			//+ 0.0 to avoid negative 0 on uoutput
+		    }
+		    System.out.print("\n");
+		}
     }
     
 }
